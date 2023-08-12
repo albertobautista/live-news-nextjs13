f0bf953f0262fee7f41eddf8a287fb970db643ce
Alberto Bautista <dachb101@gmail.com>
2023-08-11 22:40:43 -0600

    Add footer

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -15,7 +15,6 @@ const ArticlePage = ({ searchParams }: Props) => {
 
   const article: Article = searchParams!;
   return (
-    // <article>
     <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10 pt-10">
       {article.image && (
         <img
@@ -26,19 +25,32 @@ const ArticlePage = ({ searchParams }: Props) => {
       )}
       <div className="px-10">
         <h1 className="px-0 no-underline pb-2 headerTitle">{article.title}</h1>
-        <div className="flex gap-y-3 flex-col sm:flex-row sm:divide-x-2 divide-double sm:space-x-4 py-5">
-          <h2 className="font-bold text-sm">By: {article.author}</h2>
-          <h2 className="font-bold sm:pl-3 text-sm">
-            Source: {article.source}
-          </h2>
-          <p className="sm:pl-3 text-sm">
+        <div className="flex gap-y-1 flex-col divide-double py-5">
+          <div className="text-sm flex gap-1">
+            <p className="font-bold">By:</p> <p>{article.author}</p>
+          </div>
+
+          <div className="text-sm flex gap-1">
+            <p className="font-bold">Source:</p> <p>{article.source}</p>
+          </div>
+          <div className="text-sm flex gap-1">
+            <p className="font-bold">Language:</p>{" "}
+            <p>{article.language === "en" ? "🇺🇸" : "🇪🇸"}</p>
+          </div>
+          <p className="text-sm italic">
             <LiveTimestamp time={article.published_at} />
           </p>
+          <a
+            href={article.url}
+            target="_blank"
+            className="text-orange-400 font-semibold italic text-md cursor-pointer"
+          >
+            Visit website
+          </a>
         </div>
-        <p className="pt-4">{article.description}</p>
+        <p>{article.description}</p>
       </div>
     </section>
-    // </article>
   );
 };
 
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
+export { default as Footer } from "./Footer";
 export { default as LiveTimestamp } from "./LiveTimestamp";
 export { default as ReadMoreButton } from "./ReadMoreButton";
